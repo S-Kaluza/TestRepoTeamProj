b30710491a5fc52002b382619e2bca832b9fd942
Szymon Kałuża <s.kaluza02@gmail.com>
2026-05-17 10:39:19 +0200

    second

--- hello/views.py
+++ hello/views.py
@@ -12,6 +12,8 @@ def api_get_story(request):
     
     # Zaktualizowana ścieżka docelowa wskazująca na folder 'data'
     file_path = settings.BASE_DIR / 'data' / 'story.json'
+
+    print("hello world")
     
     try:
         with open(file_path, 'r', encoding='utf-8') as f:
