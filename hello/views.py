aea7640d028c3277e685b4e6747a57467479eb02
Szymon Kałuża <s.kaluza02@gmail.com>
2026-05-17 12:24:58 +0200

    KAN-3 SimpleTask3: new one line commit

--- hello/views.py
+++ hello/views.py
@@ -13,7 +13,7 @@ def api_get_story(request):
     # Zaktualizowana ścieżka docelowa wskazująca na folder 'data'
     file_path = settings.BASE_DIR / 'data' / 'story.json'
 
-    print("hello world")
+    print("hello world222")
 
     
     try:
